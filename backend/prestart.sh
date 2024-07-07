cb239bc78044218d34d1a77500df0839e3bf954c
damtrix <oludamola.onarinde@gmail.com>
2024-07-07 01:29:42 +0100

    backend

--- backend/prestart.sh
+++ backend/prestart.sh
@@ -1,13 +1,10 @@
 #! /usr/bin/env bash
 
-# Set the PYTHONPATH to the app directory
-export PYTHONPATH=/app
-
 # Let the DB start
-python ./app/app/backend_pre_start.py
+python ./app/backend_pre_start.py
 
 # Run migrations
 alembic upgrade head
 
 # Create initial data in DB
-python ./app/app/initial_data.py
+python ./app/initial_data.py
